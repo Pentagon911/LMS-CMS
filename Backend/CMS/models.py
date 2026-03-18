e5d3995d5c0fe5707a525bb25e60b3994d3bafc3
shavindaanuchana@gmail.com <shavindaanuchana@gmail.com>
2026-03-18 12:54:15 +0530

    Removed cource model in CMS created for testing

--- Backend/CMS/models.py
+++ Backend/CMS/models.py
@@ -4,13 +4,6 @@ User = get_user_model()
 
 # Create your models here.
 # yet to connect LMS sem module as foriegn key to week
-class Course(models.Model):
-    code = models.CharField(max_length=20, unique=True)
-    name = models.CharField(max_length=200)
-    description = models.TextField(blank=True)
-    
-    def __str__(self):
-        return f"{self.code}: {self.name}"
     
 class Week(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='weeks', null=True)
